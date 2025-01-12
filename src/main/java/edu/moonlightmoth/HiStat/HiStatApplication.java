884982d620eef232f077aefec2aa35089ed65bd4
moonlightmoth <ghjpa@inbox.ru>
2025-01-12 09:14:42 +0300

    asd

---
+++ src/main/java/edu/moonlightmoth/HiStat/HiStatApplication.java
@@ -0,0 +1,13 @@
+package edu.moonlightmoth.HiStat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiStatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiStatApplication.class, args);
+	}
+
+}
